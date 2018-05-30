f1620d9e48e417c544eabcaee8960043eb12fe49
xinzhedeai <2291006094@qq.com>
2018-05-31 00:01:53 +0800

    vue vue-router下

--- vue-eleme-1.2.0/sell/src/App.vue
+++ vue-eleme-1.2.0/sell/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div>
      <v-header></v-header>
     <div class="tab border-1px">
       <div class="tab-item">
---
+++ vue-eleme-1.2.0/sell/src/components/seller/seller.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="page">
+      I am seller~O(∩_∩)O
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {};
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+</style>

a8f5ce84b6cc6e68f9537636ee328b2a3d631617
xinzhedeai <2291006094@qq.com>
2018-06-11 17:30:14 +0800

    eleme-vue1.2.0
    商品详情-评论

--- vue-eleme-1.2.0/sell/src/components/food/food.vue
+++ vue-eleme-1.2.0/sell/src/components/food/food.vue
@@ -17,19 +17,53 @@
           <span class="now">￥{{food.price}}</span>
           <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
         </div>
+        <div class="cartcontrol-wrapper">
+          <cartcontrol :food="food"></cartcontrol>
+        </div>
+        <div class="buy" @click="addFirst"  v-show="!food.count || food.count === 0" transition="fade">加入购物车</div>
       </div>
-      <div class="cartcontrol-wrapper">
-        <cartcontrol :food="food"></cartcontrol>
+      <split v-show="food.info"></split>
+      <div class="info" v-show="food.info">
+        <h1 class="title">商品信息</h1>
+        <p class="text">{{food.info}}</p>
       </div>
-      <div class="buy" v-show="!food.count || food.count === 0">加入购物车</div>
+      <split v-show="food.info"></split>
+      <div class="rating">
+        <h1 class="title">商品评价</h1>
+        <ratingselect :select-type="selectType" :only-content="onlyContent"
+         :desc="desc" :ratings="food.ratings"></ratingselect>
+        <div class="rating-wrapper">
+          <ul v-show="food.ratings && food.ratings.length">
+            <li v-show="needShow(rating.rateType, rating.text)" v-for="rating in food.ratings" class="rating-item">
+              <div class="user">
+                <span class="name">{{rating.username}}</span>
+                <img :src="rating.avatar" class="avatar" width="12" height="12">
+              </div>  
+              <div class="time">{{rating.rateTime}}</div>
+              <p class="text">
+                <span :class="{'icon-thumb_up': rating.rateType === 0, 
+                'icon-thumb_down': rating.rateType === 1}"></span>{{rating.text}}
+              </p>
+            </li>
+          </ul>
+           <div class="no-ratings" v-show="!food.ratings || !food.ratings.length">
+             暂无评论
+           </div>
+        </div>
+      </div>      
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import Vue from 'vue';
   import BScroll from 'better-scroll';
   import cartcontrol from 'components/cartcontrol/cartcontrol';
-  
+  import ratingselect from 'components/ratingselect/ratingselect';
+  import split from 'components/split/split';
+  const POSITIVE = 0; // 积极评价
+  // const NEGATIVE = 1; // 消极评价
+  const ALL = 2;
   export default {
     props: { // 接受父组件传过来的值
       food: {
@@ -38,12 +72,38 @@
     },
     data() {
       return {
-        showFlag: false
+        showFlag: false,
+        selectType: ALL,
+        onlyContent: true,
+        desc: {
+          all: '全部',
+          positive: '推荐',
+          negative: '吐槽'
+        }
       };
     },
     methods: {
+      needShow(type, text) { // 评论和上面的只看有内容的div文字显示与隐藏控制
+        if (this.onlyContent && !text) {
+          return false;
+        }
+        if (this.selectType === ALL) {
+          return true;
+        } else {
+          return type === this.selectType;
+        }
+      },
+      addFirst(event) { // 商品详情添加购物车
+        if (!event._constructed) { // 如果不是vue自己的点击事件，则不处理
+          return;
+        }
+        this.$dispatch('cart.add', event.target);
+        Vue.set(this.food, 'count', 1); // 将food属性count的值设为1
+      },
       show() {
         this.showFlag = true;
+        this.selectType = POSITIVE;
+        this.onlyContent = true;
         // console.info('**显示嘿嘿**');
         this.$nextTick(() => { // 当组件dom健在完毕后进行操作
           if (!this.scroll) {
@@ -59,13 +119,30 @@
         this.showFlag = false;
       }
     },
+    events: { // 监听子组件派送过来的时间，并触发
+      'ratingtype.select'(type) {
+        this.selectType = type;
+        this.$nextTick(() => {
+          this.scroll.refresh();
+        });
+      },
+      'content.toggle'(onlyContent) {
+        this.onlyContent = onlyContent;
+        this.$nextTick(() => {
+          this.scroll.refresh();
+        });
+      }
+    },
     components: {
-      cartcontrol
+      cartcontrol,
+      split,
+      ratingselect
     }
   };
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
+  @import '../../common/stylus/mixin.styl';
   .food
     position fixed
     left 0
@@ -102,6 +179,7 @@
           font-size 20px
           color #fff
     .content
+      position relative
       padding 18px
       .title
         line-height 14px
@@ -119,7 +197,7 @@
           color rgb(147, 153, 159)
         .sell-count
           margin-right 12px
-       .price
+        .price
           font-weight 700
           line-height 24px
           .now
@@ -130,22 +208,89 @@
             text-decoration line-through
             font-size 10px
             color rgb(147, 153, 159)
-    .cartcontrol-wrapper
-      position absolute
-      right 12px
-      bottom 12px
-    .buy
-      position absolute
-      right 18px
-      bottom 18px
-      z-index 10
-      height 24px
-      line-height 24px
-      padding: 0 12px;
-      background-color rgb(0, 160, 220)
-      box-sizing border-box
-      font-size 10px
-      color #fff
-      border-radius 12px
+      .cartcontrol-wrapper
+        position absolute
+        right 20px
+        bottom 18px
+      .buy
+        position absolute
+        right 18px
+        bottom 18px
+        z-index 10
+        height 24px
+        line-height 24px
+        padding: 0 12px;
+        background-color rgb(0, 160, 220)
+        box-sizing border-box
+        font-size 10px
+        color #fff
+        border-radius 12px
+        &.fade-transition
+          transition all .2s
+          opacity 1
+        &.fade-enter, &.fade-leave
+          opacity 0
+    .info 
+      padding 18px
+      .title
+        line-height 14px
+        margin-bottom 6px
+        font-size 14px
+        color rgb(77, 17, 27)
+      .text
+        line-height 24px
+        padding: 0 8px
+        font-size 12px
+        color rgb(77, 85, 93)
+    .rating
+      padding-top: 18px
+      .title
+        line-height 14px
+        margin-left 18px
+        font-size 14px
+        color rgb(7, 17, 27)
+      .rating-wrapper // 评论区样式
+        padding 0 18px
+        .rating-item
+          position relative
+          padding 16px 0
+          border-1px(rgba(7, 17, 27, .1))
+          .user
+            position absolute
+            right 0
+            top 16px
+            line-height 12px
+            font-size 0
+            .name 
+              display inline-block
+              margin-right 6px
+              vertical-align top
+              font-size 10px
+              color rgb(147, 153, 159)
+            .avatar
+              border-radius 50%
+          .time
+            margin-bottom 6px
+            line-height 12px
+            font-size 10px
+            color rgb(147, 153, 159)
+          .text
+            line-height 16px
+            font-size 12px
+            color rgb(7, 17, 27)
+            .icon-thumb_up, .icon-thumb_down
+              margin-right 4px
+              line-height 24px
+              font-size 12px
+            .icon-thumb_up
+              color rgb(0, 160, 220)
+            .icon-thumb_down // 取消点赞
+              color rgb(147, 153,159)
+        .no-ratings
+          padding 16px 0
+          font-size 12px
+          color rgb(147, 153, 159)
+              
+
 
 </style>

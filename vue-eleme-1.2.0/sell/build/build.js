321fda370f0844653b9454f298184645b99c0c1f
xinzhedeai <2291006094@qq.com>
2018-06-21 10:28:25 +0800

    vue-eleme-1.2.0
    项目构建以及部署测试

--- vue-eleme-1.2.0/sell/build/build.js
+++ vue-eleme-1.2.0/sell/build/build.js
@@ -1,10 +1,10 @@
 // https://github.com/shelljs/shelljs
-require('shelljs/global')
+require('shelljs/global') // 在可以在nodejs中执行常用的shell命令
 env.NODE_ENV = 'production'
 
 var path = require('path')
 var config = require('../config')
-var ora = require('ora')
+var ora = require('ora') // 长时间任务执行提示
 var webpack = require('webpack')
 var webpackConfig = require('./webpack.prod.conf')
 
@@ -14,7 +14,7 @@ console.log(
   '  Opening index.html over file:// won\'t work.\n'
 )
 
-var spinner = ora('building for production...')
+var spinner = ora('building for production...') // 获取长期执行的任务对其进行提示
 spinner.start()
 
 var assetsPath = path.join(config.build.assetsRoot, config.build.assetsSubDirectory)
---
+++ vue-eleme-1.2.0/sell/prod.server.js
@@ -0,0 +1,60 @@
+var express = require('express'); // 移入express模块
+var config = require('./config/index');
+// 配置端口号
+var port = process.env.port || config.build.port;
+// 创建一个server服务器
+var app = express();
+// 声明路由对象
+var router = express.Router();
+
+router.get('/', function (req, res, next) {
+  req.url = '/index.html';
+  next();
+});
+
+app.use(router);
+
+var appData = require('./data.json');
+var seller = appData.seller;
+var goods = appData.goods;
+var ratings = appData.ratings;
+
+var apiRoutes = express.Router();
+apiRoutes.get('/seller', (req, res) => {
+  res.json({
+    errno: 0,
+    data: seller
+  });
+});
+
+apiRoutes.get('/goods', (req, res) => {
+  res.json({
+    errno: 0,
+    data: goods
+  });
+});
+
+apiRoutes.get('/ratings', (req, res) => {
+  res.json({
+    errno: 0,
+    data: ratings
+  });
+});
+
+app.use('/api', apiRoutes);
+
+/**
+ * 执行npm build生成dist目录
+ * 将dist下打包好的文件
+ * 部署到express的服务器上
+ * 
+ **/
+app.use(express.static('./dist'));
+
+module.exports = app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('Listening at http://localhost:' + port + '\n');
+});
